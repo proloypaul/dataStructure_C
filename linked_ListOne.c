3563ce54cb9f1a6bbf4ca20c77a5493a1cde81b5
proloypaul <proloyp23@gmail.com>
2022-12-19 23:56:13 +0600

    remove node

--- linked_ListOne.c
+++ linked_ListOne.c
@@ -11,7 +11,7 @@ struct node{
     int data;
     Node *next;
 };
-
+// create node 
 Node *create_node(int item, Node *next){
     Node *new_node = (Node *) malloc(sizeof(Node));
     if(new_node == NULL){
@@ -21,6 +21,30 @@ Node *create_node(int item, Node *next){
     new_node->data = item;
     new_node->next = next;
     return new_node;
+}
+
+// remove node
+Node *remove_node(Node *head, Node *node){
+    if(head == node){
+        head = node->next;
+        free(node);
+        return head;
+    }
+
+    Node *current_node = head;
+
+    while(current_node != NULL){
+        if(current_node->next == node){
+            break;
+        }
+        current_node = current_node->next;
+    }
+    if(current_node == NULL){
+        return head;
+    }
+    current_node->next = node->next;
+    free(node);
+    return head;
 }
 
 int main(){
